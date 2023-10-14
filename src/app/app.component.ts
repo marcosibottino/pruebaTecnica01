9671a1c208d06bb548db8d1dd0366669bae4f59c
marcosbottino <mbottino@hms-tech.com>
2023-10-14 17:39:47 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { books } from '../assets/data/books.json'
+import { MatSnackBar } from '@angular/material/snack-bar'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pruebaTecnica01';
+
+  public books: any = books.library;
+
+  public lectures:any = [];
+
+  public length: any;
+
+  book: any;
+
+  constructor(private snackBar: MatSnackBar){}
+
+  ngOnInit(){
+    this.lengthBooks();
+
+    console.log('Libros: ',this.books)
+  }
+
+  addLecture(book: any) {
+      console.log(book);
+      if(this.lectures.includes(book)){
+        console.log('Libro YA agregado.');
+      } else {
+        this.lectures.push(book);
+        console.log('Lecture', this.lectures);
+      }
+  }
+
+  deleteLecture(book:any){
+    this.lectures.splice(book, 1);
+    console.log(this.lectures);
+    
+  }
+
+  lengthBooks(){
+    const books = this.books;
+
+    this.length = books.length;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import { ToastModule } from 'ng2-toastr';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
